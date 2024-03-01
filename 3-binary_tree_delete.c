96155d013d7fd4085debad1e94786fdbf727d1e0
itsSamarIbrahim <>
2024-02-29 16:07:10 -0800

    added

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -3,17 +3,17 @@
 /**
  * binary_tree_delete - a function that deletes an entire binary tree
  * @tree: the tree to be deleted
- * 
+ *
  * Return: void
-*/
+ */
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-        if (tree == NULL)
-                return;
+	if (tree == NULL)
+		return;
 
-        binary_tree_delete(tree->left);
-        binary_tree_delete(tree->right);
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
 
-        free(tree);
+	free(tree);
 }
