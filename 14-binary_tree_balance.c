17b6816b8fb5e15acb17661a8e97f62dad40cf6b
Samar <bantmasr21@gmail.com>
2024-03-01 04:21:41 +0200

    added

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -6,15 +6,23 @@
 
 size_t binary_tree_height_2(const binary_tree_t *tree)
 {
-	size_t left = 0, right = 0;
+	int left = 1, right = 1;
 
-	if (!tree)
+	if (tree)
+	{
+		if (tree->right)
+			right += binary_tree_height_2(tree->right);
+
+		if (tree->left)
+			left += binary_tree_height_2(tree->left);
+
+		if (left > right)
+			return (left);
+		else
+			return (right);	
+	}
+	else
 		return (0);
-
-	left = tree->left ? 1 + binary_tree_height_2(tree->left) : 0;
-	right = tree->right ? 1 + binary_tree_height_2(tree->right) : 0;	
-
-	return ((left > right) ? left : right);
 }
 
 int binary_tree_balance(const binary_tree_t *tree)
